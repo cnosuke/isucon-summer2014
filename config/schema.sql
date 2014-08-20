478e3997b5f5176479191bf7a1b5af246479003c
Shinnosuke Takeda <shinnosuke-takeda@cookpad.com>
2014-08-20 15:33:32 +0900

    Update schema

--- config/schema.sql
+++ config/schema.sql
@@ -6,7 +6,11 @@ CREATE TABLE `memos` (
   `is_private` tinyint(4) NOT NULL DEFAULT '0',
   `created_at` datetime NOT NULL,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idx_is_private_created_at_id` (`is_private`,`created_at`,`id`),
+  UNIQUE KEY `idx_user_created_at` (`user`,`created_at`),
+  KEY `idx_is_private` (`is_private`),
+  KEY `idx_is_private_created_at` (`is_private`,`created_at`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `users`;
@@ -15,7 +19,7 @@ CREATE TABLE `users` (
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `salt` varchar(255) NOT NULL,
-  `last_access` datetime,
+  `last_access` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `users_username_idx` (`username`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
