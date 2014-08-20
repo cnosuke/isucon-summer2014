c6c9495949651eaed3deed3797dd20a3803ab450
Shinnosuke Takeda <shinnosuke-takeda@cookpad.com>
2014-08-20 15:47:20 +0900

    Add make_index

---
+++ init_script/make_index.sql
@@ -0,0 +1,3 @@
+create index idx_is_private on memos (is_private);
+create unique index idx_is_private_created_at_id on memos (is_private, created_at, id);
+create unique index idx_user_created_at on memos (user, created_at);
