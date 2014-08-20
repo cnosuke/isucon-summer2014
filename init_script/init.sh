928b01112b6d87b15af0a7480223153d6813691d
cnosuke <shinnosuke@gmail.com>
2014-08-20 15:49:52 +0900

    hotfix

--- init_script/init.sh
+++ init_script/init.sh
@@ -3,4 +3,4 @@ set -e
 
 #データ投入後になにかしらの作業をしたい場合はこのシェルスクリプトに書いてください
 
-mysql -P 3306 -u isucon < /home/isu-user/isucon/webapp/ruby/init_script/make_index
+mysql -P 3306 -u isucon < /home/isu-user/isucon/webapp/ruby/init_script/make_index.sql
