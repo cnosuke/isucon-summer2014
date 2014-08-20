5c5795111cad10759aa71607ea61dfa8b6fcf358
Shinnosuke Takeda <shinnosuke-takeda@cookpad.com>
2014-08-20 16:37:09 +0900

    Read from markdown

--- app.rb
+++ app.rb
@@ -79,8 +79,14 @@ class Isucon3App < Sinatra::Base
     mysql = connection
     user  = get_user
 
-    total = mysql.query("SELECT count(*) AS c FROM memos WHERE is_private=0").first["c"]
-    memos = mysql.query("SELECT * FROM memos WHERE is_private=0 ORDER BY created_at DESC, id DESC LIMIT 100")
+    total = mysql.query(
+      "SELECT count(*) AS c FROM memos WHERE is_private=0"
+      ).first["c"]
+
+    memos = mysql.query(
+      "SELECT * FROM memos WHERE is_private=0 ORDER BY created_at DESC, id DESC LIMIT 100"
+      )
+
     memos.each do |row|
       row["username"] = mysql.xquery("SELECT username FROM users WHERE id=?", row["user"]).first["username"]
     end
@@ -164,7 +170,7 @@ class Isucon3App < Sinatra::Base
     mysql = connection
     user  = get_user
 
-    memo = mysql.xquery('SELECT id, user, content, is_private, created_at, updated_at FROM memos WHERE id=?', params[:memo_id]).first
+    memo = mysql.xquery('SELECT id, user, content, content_html, is_private, created_at, updated_at FROM memos WHERE id=?', params[:memo_id]).first
     unless memo
       halt 404, "404 Not Found"
     end
@@ -174,7 +180,7 @@ class Isucon3App < Sinatra::Base
       end
     end
     memo["username"] = mysql.xquery('SELECT username FROM users WHERE id=?', memo["user"]).first["username"]
-    memo["content_html"] = gen_markdown(memo["content"])
+    memo["content_html"] = memo["content_html"]
     if user["id"] == memo["user"]
       cond = ""
     else
@@ -208,9 +214,10 @@ class Isucon3App < Sinatra::Base
     anti_csrf
 
     mysql.xquery(
-      'INSERT INTO memos (user, content, is_private, created_at) VALUES (?, ?, ?, ?)',
+      'INSERT INTO memos (user, content, content_html, is_private, created_at) VALUES (?, ?, ?, ?)',
       user["id"],
       params["content"],
+      gen_markdown(params["content"]),
       params["is_private"].to_i,
       Time.now,
     )
